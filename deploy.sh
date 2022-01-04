3aa5cbd4b8e0d375c93bab2166d9ada3a56ca0e4
Andy8647 <andy8647lgc@gmail.com>
2022-01-04 12:12:37 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Andy8647/theme-watcher.git main:gh-pages
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/theme-watcher/",
   plugins: [svelte()]
 })
