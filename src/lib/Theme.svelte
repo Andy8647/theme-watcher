8e7104f2b60daa10c31f473444397e72cb6cbfd2
Andy8647 <andy8647lgc@gmail.com>
2022-01-04 11:55:46 -0500

    init

---
+++ src/lib/Theme.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let darkFlag: MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+    let theme: string = darkFlag.matches ? "dark" : "light";
+    darkFlag.addEventListener("change", e => {
+        theme = e.matches ? "dark" : "light";
+    })
+</script>
+
+<p>
+    Current theme: {theme}
+</p>
